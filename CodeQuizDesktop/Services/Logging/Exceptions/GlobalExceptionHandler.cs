c9141ea75d1c6cd2d56faf3ca27c1ad62f5b49e1
Abdomohamed16 <abdomohas18@gmail.com>
2025-11-29 22:57:31 +0200

    Update GlobalExceptionHandler to show snackbar messages

--- CodeQuizDesktop/Services/Logging/Exceptions/GlobalExceptionHandler.cs
+++ CodeQuizDesktop/Services/Logging/Exceptions/GlobalExceptionHandler.cs
@@ -1,22 +1,32 @@
-using System;
 using CodeQuizDesktop.Services.Logging;
+using CodeQuizDesktop.Services.UI;
 
 namespace CodeQuizDesktop.Services.Exceptions;
 
 public class GlobalExceptionHandler
 {
     private readonly AppLogger<GlobalExceptionHandler> _logger;
+    private readonly ISnackBarService _snackBar;
 
-    public GlobalExceptionHandler(AppLogger<GlobalExceptionHandler> logger)
+    public GlobalExceptionHandler(
+        AppLogger<GlobalExceptionHandler> logger,
+        ISnackBarService snackBar)
     {
         _logger = logger;
+        _snackBar = snackBar;
 
         AppDomain.CurrentDomain.UnhandledException += HandleUnhandled;
     }
 
-    private void HandleUnhandled(object sender, UnhandledExceptionEventArgs e)
+    private async void HandleUnhandled(object sender, UnhandledExceptionEventArgs e)
     {
         var ex = e.ExceptionObject as Exception;
+
         _logger.LogError("Unhandled exception", ex);
+
+        await _snackBar.ShowAsync(
+            "An unexpected error occurred ❌",
+            SnackBarType.Error
+        );
     }
 }
