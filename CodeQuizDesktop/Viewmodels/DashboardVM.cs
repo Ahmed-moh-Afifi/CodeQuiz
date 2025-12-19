420153360d4cc06c85143e39aca5cccea4760804
Abdomohamed16 <abdomohas18@gmail.com>
2025-12-19 18:18:04 +0200

    add Dashboard view model skeleton

--- CodeQuizDesktop/Viewmodels/DashboardVM.cs
+++ CodeQuizDesktop/Viewmodels/DashboardVM.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CodeQuizDesktop.Models;
+using CodeQuizDesktop.Repositories;
+using System.Collections.ObjectModel;
 
 namespace CodeQuizDesktop.Viewmodels
 {
     public class DashboardVM : BaseViewModel
     {
+        private ObservableCollection<ExamineeAttempt> joinedAttempts;
+        public ObservableCollection<ExamineeAttempt> JoinedAttempts
+        {
+            get => joinedAttempts;
+            set { joinedAttempts = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<ExaminerQuiz> createdQuizzes;
+        public ObservableCollection<ExaminerQuiz> CreatedQuizzes
+        {
+            get => createdQuizzes;
+            set { createdQuizzes = value; OnPropertyChanged(); }
+        }
+
         public DashboardVM()
         {
+            JoinedAttempts = new ObservableCollection<ExamineeAttempt>();
+            CreatedQuizzes = new ObservableCollection<ExaminerQuiz>();
         }
     }
 }
